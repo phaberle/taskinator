2ef53101270b34afdb8d208f293533b3c358c70f
Paul Haberle <61989020+phaberle@users.noreply.github.com>
2022-04-07 10:59:42 -0500

    made inputs, then place results into <ul>

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,26 @@
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
+/*For appending, start from inside and go out as you call append child*/
 var createTaskHandler = function(event) {
     event.preventDefault();
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
-    var taskItemEl = document.createElement("li");
-    taskItemEl.className = "task-item";
-    taskItemEl.textContent = "This is a new task.";
-    tasksToDoEl.appendChild(taskItemEl);
+    //create list item
+    var listItemEl = document.createElement("li");
+    listItemEl.className = "task-item";
+
+    //create div to hold task info and add to list item
+    var taskInfoEl = document.createElement("div");
+    taskInfoEl.className = 'task-info';
+
+    //add HTML content to div
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    listItemEl.appendChild(taskInfoEl);
+
+    //add entire list item to list
+    tasksToDoEl.appendChild(listItemEl);
 };
 
 formEl.addEventListener("submit", createTaskHandler);
