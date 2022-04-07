0cb160bd1e0c5afe3cffbc49b12dee12915a5d4c
Paul Haberle <61989020+phaberle@users.noreply.github.com>
2022-04-07 00:38:31 -0500

    added form

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,13 @@
-var buttonE1 = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function() {
+var createTaskHandler = function(event) {
+    event.preventDefault();
+
     var taskItemEl = document.createElement("li");
     taskItemEl.className = "task-item";
     taskItemEl.textContent = "This is a new task.";
     tasksToDoEl.appendChild(taskItemEl);
 };
 
-buttonE1.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
